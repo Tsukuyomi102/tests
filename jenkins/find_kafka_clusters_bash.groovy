db9c70d8e6c7d74270e7b5085eccbbc7bb5e514c
Egor Svirin <57195454+Tsukuyomi102@users.noreply.github.com>
2025-10-23 10:24:11 +0300

    some tests

--- jenkins/find_kafka_clusters_bash.groovy
+++ jenkins/find_kafka_clusters_bash.groovy
@@ -5,14 +5,13 @@ def call() {
 set -euo pipefail
 
 yq e -o=tsv '
-  ..                                                     # пройти по всем нодам
-  | select(tag == "!!map")                               # оставить только мапы
-  | to_entries[]                                         # развернуть в пары {key, value}
-  | select( .value
-            | tag == "!!str"
-            and contains("kafka_fp")
-            and test("\\.json$") )                       # фильтр значения
-  | .key                                                 # вывести имя ключа
+  ..                          # обойти все узлы
+  | select(tag == "!!map")    # оставить только мапы
+  | to_entries[]              # пары {key, value}
+  | select(.value | tag == "!!str")         # строго строки
+  | select(.value | contains("kafka_fp"))   # содержит kafka_fp
+  | select(.value | test("\\\\.json$"))     # заканчивается на .json
+  | .key                      # вывести имя ключа
 ' conf/distrib.yml | sort -u
 ''',
     returnStdout: true
