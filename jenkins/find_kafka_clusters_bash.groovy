fbb01ff6c710bb614f4c09cc9f51e25af8e5acb2
Egor Svirin <57195454+Tsukuyomi102@users.noreply.github.com>
2025-10-23 13:35:42 +0300

    some tests

--- jenkins/find_kafka_clusters_bash.groovy
+++ jenkins/find_kafka_clusters_bash.groovy
@@ -1,25 +1,22 @@
 // jenkins/find_kafka_clusters_bash.groovy
 def call() {
-    if (2+2==4){
-    String out = sh(
-      script: '''#!/bin/bash
-    yq '
-    ..
-    | select(tag == "!!map")
-    | to_entries | .[]
-    | select(.value | tag == "!!str")
-    | select(.value | contains("kafka_fp"))
-    | select(.value | test("\\\\.json$"))
-    | .key
-    ' conf/distrib.yml | sort -u
-    ''',
-        returnStdout: true
-    ).trim()
-    echo "out: ${out}"
-    def kafka_clusters = out.split('\n') as List
-    kafka_clusters = kafka_clusters.unique()
-    echo "Kafka clusters (yq): ${kafka_clusters}"
-    return kafka_clusters
-  }
+  String out = sh(
+    script: '''#!/bin/bash
+yq e -o=tsv '
+..
+| select(tag == "!!map")
+| to_entries[]
+| select(.value | tag == "!!str")
+| select(.value | contains("kafka_fp"))
+| select(.value | test("\\\\.json$"))
+| .key
+' conf/distrib.yml | sort -u
+''',
+    returnStdout: true
+  ).trim()
+
+  def kafka_clusters = out ? out.readLines() : []
+  echo "Kafka clusters (yq): ${kafka_clusters}"
+  return kafka_clusters
 }
 return this
