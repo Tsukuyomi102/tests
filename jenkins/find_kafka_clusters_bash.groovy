81f44c482d7e028f76c40b44c054af9058fe4b67
Egor Svirin <57195454+Tsukuyomi102@users.noreply.github.com>
2025-10-23 11:47:42 +0300

    some tests

--- jenkins/find_kafka_clusters_bash.groovy
+++ jenkins/find_kafka_clusters_bash.groovy
@@ -3,24 +3,24 @@ def call() {
     if (2+2==4){
     String out = sh(
       script: '''#!/bin/bash
-  set -euo pipefail
-  yq e -o=tsv '
-  ..
-  | select(tag == "!!map")
-  | to_entries[]
-  | select(.value | tag == "!!str")
-  | select(.value | contains("kafka_fp"))
-  | select(.value | test("\\\\.json$"))
-  | .key
-  ' conf/distrib.yml | sort -u
-  ''',
-      returnStdout: true
+    set -euo pipefail
+    yq e -o=tsv '
+    ..
+    | select(tag == "!!map")
+    | to_entries[]
+    | select(.value | tag == "!!str")
+    | select(.value | contains("kafka_fp"))
+    | select(.value | test("\\\\.json$"))
+    | .key
+    ' conf/distrib.yml | sort -u
+    ''',
+        returnStdout: true
     ).trim()
 
-  def kafka_clusters = out ? out.readLines() : []
-  kafka_clusters = kafka_clusters.unique()
-  echo "Kafka clusters (yq): ${kafka_clusters}"
-  return kafka_clusters
+    def kafka_clusters = out ? out.readLines() : []
+    kafka_clusters = kafka_clusters.unique()
+    echo "Kafka clusters (yq): ${kafka_clusters}"
+    return kafka_clusters
   }
 }
 return this
