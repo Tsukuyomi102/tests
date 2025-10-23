3a7c41eeb9ca453664973da682ae62f0d2970b5a
Egor Svirin <57195454+Tsukuyomi102@users.noreply.github.com>
2025-10-23 13:37:58 +0300

    first commit

--- jenkins/find_kafka_clusters_bash.groovy
+++ jenkins/find_kafka_clusters_bash.groovy
@@ -8,12 +8,13 @@ yq '
 | to_entries | .[]
 | select(.value | tag == "!!str")
 | select(.value | contains("kafka_fp"))
+| select(.value | test("\\\\.json$"))
 | .key
 ' conf/distrib.yml | sort -u
 ''',
     returnStdout: true
   ).trim()
-  echo ${out}
+  echo "OUT: ${out}"
   def kafka_clusters = out ? out.readLines() : []
   echo "Kafka clusters (yq): ${kafka_clusters}"
   return kafka_clusters
