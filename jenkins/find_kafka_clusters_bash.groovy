3852aaba248cdbbb30b62ff4b77aad0da8166790
Egor Svirin <57195454+Tsukuyomi102@users.noreply.github.com>
2025-10-23 13:41:44 +0300

    first commit

--- jenkins/find_kafka_clusters_bash.groovy
+++ jenkins/find_kafka_clusters_bash.groovy
@@ -8,7 +8,7 @@ yq e -o=tsv '
 | to_entries | .[]
 | select(.value | tag == "!!str")
 | select(.value | contains("kafka_fp"))
-| select(.value | test("\\\\.json$"))
+| select(.value | test("\\.json$"))
 | .key
 ' conf/distrib.yml | sort -u
 ''',
